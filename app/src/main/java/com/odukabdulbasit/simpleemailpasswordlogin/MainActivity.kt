e544648fadb60397aad788ae36dfed23ef7b1658
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-12 10:48:16 +0300

    Created code part

--- app/src/main/java/com/odukabdulbasit/simpleemailpasswordlogin/MainActivity.kt
+++ app/src/main/java/com/odukabdulbasit/simpleemailpasswordlogin/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.odukabdulbasit.simpleemailpasswordlogin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (isValidCredentials(username, password)) {
+                // Successful login
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                // Add your required logic here (e.g., go to the next activity)
+            } else {
+                // Failed login
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        // Replace this with your actual login validation logic
+        val validUsername = "your_username"
+        val validPassword = "your_password"
+        return username == validUsername && password == validPassword
     }
 }
+
